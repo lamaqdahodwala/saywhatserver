a360df6861c32966a1011f4201468d3db5d88a69
Lamaq Dahodwala <ldahodwala@wtsdnj.com>
2021-02-25 16:59:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, Response, request
+from flask_cors  import CORS
+from replit import db
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return ''
+
+@app.route('/newestposts')
+def newestposts():
+    
+    top10 = [db.get(i) for i in range(10)]
+    string = str(top10)
+    string = string[1:-1]
+    resp = Response(string)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+
+
+@app.route('/exists/<key>')
+def exists(key):
+    if key in list(db.keys()):
+        resp = Response('True')
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+    else:
+        resp = Response('False')
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+    
+    return resp
+
+
+app.run('0.0.0.0')
