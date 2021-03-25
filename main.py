f8bf7693d10d92b15946e4dbe00d1a8d81c75599
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-03-24 20:12:34 -0400

    framework

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 from flask import Flask, request
 
+
+app = Flask('app')
+
+@app.route("/")
+def home():
+    return ''
+
+@app.route("/newestposts")
+def getnewestposts():
+    pass
+
+
+@app.route('/post', methods=['POST'])
+def post():
+    pass
+
+@app.route("/read/<id>")
+def readerview(id):
+    pass
+
+@app.after_request
+def after_request():
+    request.headers['Access-Control-Allow-Origin'] = '*'
+
+app.run('0.0.0.0', port=8080)
